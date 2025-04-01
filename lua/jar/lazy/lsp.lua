5c3175b530833b20c47522144f9aac3f32a1ced2
Jaracah Teague <jaracahteague@gmail.com>
2025-03-31 21:47:07 -0400

    update lsp for go and move conform

--- lua/jar/lazy/lsp.lua
+++ lua/jar/lazy/lsp.lua
@@ -1,7 +1,6 @@
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
-        "stevearc/conform.nvim",
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
         "hrsh7th/cmp-nvim-lsp",
@@ -15,10 +14,6 @@ return {
     },
 
     config = function()
-        require("conform").setup({
-            formatters_by_ft = {
-            }
-        })
         local cmp = require('cmp')
         local cmp_lsp = require("cmp_nvim_lsp")
         local capabilities = vim.tbl_deep_extend(
@@ -41,8 +36,37 @@ return {
                         capabilities = capabilities
                     }
                 end,
-
-                zls = function()
+                ["gopls"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.gopls.setup({
+                        on_attach = function(client, bufnr)
+                            vim.api.nvim_create_autocmd("BufWritePre", {
+                                buffer = bufnr,
+                                callback = function()
+                                    local params = vim.lsp.util.make_range_params()
+                                    params.context = { only = { "source.organizeImports" } }
+                                    -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+                                    -- machine and codebase, you may want longer. Add an additional
+                                    -- argument after params if you find that you have to write the file
+                                    -- twice for changes to be saved.
+                                    -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+                                    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+                                    for cid, res in pairs(result or {}) do
+                                        for _, r in pairs(res.result or {}) do
+                                            if r.edit then
+                                                local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or
+                                                "utf-16"
+                                                vim.lsp.util.apply_workspace_edit(r.edit, enc)
+                                            end
+                                        end
+                                    end
+                                    vim.lsp.buf.format({ async = false })
+                                end,
+                            })
+                        end,
+                    })
+                end,
+                ["zls"] = function()
                     local lspconfig = require("lspconfig")
                     lspconfig.zls.setup({
                         root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
@@ -56,7 +80,6 @@ return {
                     })
                     vim.g.zig_fmt_parse_errors = 0
                     vim.g.zig_fmt_autosave = 0
-
                 end,
                 ["lua_ls"] = function()
                     local lspconfig = require("lspconfig")
@@ -111,4 +134,3 @@ return {
         })
     end
 }
-
