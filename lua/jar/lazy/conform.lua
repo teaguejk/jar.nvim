094c47d10cf8e51a02ac95f6d313be13101b4bf1
Jaracah Teague <jaracahteague@gmail.com>
2026-06-27 21:15:27 -0400

    update conform config

--- lua/jar/lazy/conform.lua
+++ lua/jar/lazy/conform.lua
@@ -1,40 +1,31 @@
 return {
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
-    keys = {
-      {
-        '<leader>f',
-        function()
-          require('conform').format { async = true, lsp_format = 'fallback' }
-        end,
-        mode = '',
-        desc = '[F]ormat buffer',
-      },
-    },
-    opts = {
-      notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          return nil
-        else
-          return {
-            timeout_ms = 500,
-            lsp_format = 'fallback',
-          }
-        end
-      end,
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
-      },
-    },
- }
+    "stevearc/conform.nvim",
+    opts = {},
+    config = function()
+        require("conform").setup({
+            format_on_save = {
+                timeout_ms = 5000,
+                lsp_format = "fallback",
+            },
+            formatters_by_ft = {
+                c = { "clang-format" },
+                cpp = { "clang-format" },
+                lua = { "stylua" },
+                go = { "gofmt" },
+                odin = { "odinfmt" },
+                javascript = { "prettier" },
+                typescript = { "prettier" },
+                elixir = { "mix" },
+            },
+            formatters = {
+                ["clang-format"] = {
+                    prepend_args = { "-style=file", "-fallback-style=LLVM" },
+                },
+            },
+        })
+
+        vim.keymap.set("n", "<leader>f", function()
+            require("conform").format({ bufnr = 0 })
+        end)
+    end,
+}
